00b528eb9e872c46563c14383b6df9895436d3f2
mo3az.abdellatif <mo3az.abdellatif@gmail.com>
2020-01-06 15:48:08 +0200

    sections

--- js/main.js
+++ js/main.js
@@ -1,38 +1,103 @@
-$(function () {
-    jQuery('img.svg').each(function () {
-        var $img = jQuery(this);
-        var imgID = $img.attr('id');
-        var imgClass = $img.attr('class');
-        var imgURL = $img.attr('src');
-
-        jQuery.get(imgURL, function (data) {
-            // Get the SVG tag, ignore the rest
-            var $svg = jQuery(data).find('svg');
-
-            // Add replaced image's ID to the new SVG
-            if (typeof imgID !== 'undefined') {
-                $svg = $svg.attr('id', imgID);
-            }
-            // Add replaced image's classes to the new SVG
-            if (typeof imgClass !== 'undefined') {
-                $svg = $svg.attr('class', imgClass + ' replaced-svg');
-            }
-
-            // Remove any invalid XML tags as per http://validator.w3.org
-            $svg = $svg.removeAttr('xmlns:a');
-
-            // Check if the viewport is set, else we gonna set it if we can.
-            if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-            }
-
-            // Replace image with new SVG
-            $img.replaceWith($svg);
-
-        }, 'xml');
-
+$(window).on("load", function () {
+    $(".loader").fadeOut(500, function () {
+        $('body').css("overflow", "visible");
+        $('body').animate({
+            scrollTop: 0
+        }, 1);
     });
 });
+$(document).ready(function () {
 
-$(window).on("load", function () { });
-$(document).ready(function () { });
+    if ($(window).width() <= 767) {
+        $(".nav-foot-header").addClass("mo-accordion");
+        $(".nav-foot").addClass("mo-panel");
+
+        $('.mo-menu-icon').click(function () {
+            $(".navbar-cont").fadeIn(400);
+            $(".nav-ul-cont").addClass("nav-in");
+            $("body").toggleClass("overflow");
+            $(".lang").show();
+        });
+        $('.mo-search-icon').click(function () {
+            $(".search-form").fadeIn(500);
+            $("body").toggleClass("overflow");
+            $('.search-input').focus();
+        });
+        $('.navbar-cont, .close-btn, .search-form').click(function () {
+            $(".navbar-cont").fadeOut(400);
+            $(".nav-ul-cont").removeClass("nav-in");
+            $("body").toggleClass("overflow");
+            $(".search-form").fadeOut(500);
+            $(".lang").hide();
+        });
+        $('.mo-navbar, .search-cont').click(function (e) {
+            e.stopPropagation();
+        });
+
+    }
+    $('.mo-accordion').click(function () {
+        var x = $(this).siblings().prop('scrollHeight') + 12 + "px";
+        $(".mo-accordion").not(this).removeClass("active");
+        $(this).toggleClass("active");
+        if ($(this).siblings().css('max-height') == '0px') {
+            $(this).siblings().css('max-height', x);
+            $(this).siblings().css('padding-top', "15px");
+        } else {
+            $(this).siblings().css('max-height', '0');
+            $(this).siblings().css('padding-top', "0");
+        }
+
+        $(".mo-accordion").not(this).siblings().css('max-height', '0');
+        $(".mo-accordion").not(this).siblings().css('padding-top', "0");
+    })
+
+
+
+    var mainswiper = new Swiper('.main-slider .swiper-container', {
+        pagination: {
+            el: '.main-slider .swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.main-slider .swiper-button-next',
+            prevEl: '.main-slider .swiper-button-prev',
+        },
+        autoplay: {
+            delay: 5000,
+        },
+        spaceBetween: 10,
+        loop: true,
+        breakpoints: {
+            500: {
+                navigation: false
+            },
+        }
+    });
+
+    var productsswiper = new Swiper('.products .Product-slider', {
+        slidesPerView: 5,
+        spaceBetween: 10,
+        loop: true,
+        navigation: {
+            nextEl: '.products .swiper-button-next',
+            prevEl: '.products .swiper-button-prev',
+        },
+        breakpoints: {
+            500: {
+                slidesPerView: 1,
+                pagination: {
+                    el: '.products .swiper-pagination',
+                    clickable: true,
+                },
+            },
+            991: {
+                slidesPerView: 3,
+                navigation: false,
+            },
+            1200: {
+                slidesPerView: 4,
+            }
+        }
+    });
+
+});
